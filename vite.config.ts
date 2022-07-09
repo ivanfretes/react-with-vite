3b9350c4ee5be53732de7b85e8585fbf5af9acd3
Iván Fretes <vanfretes@gmail.com>
2022-07-09 15:49:10 -0400

    change base name to repository name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: 'https://ivanfretes.github.io/',
+   base: 'https://ivanfretes.github.io/react-with-vite/',
    plugins: [react()]
 })
